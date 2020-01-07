32f2eb4582a2f21a7581babe75c83fff812d34c4
kevinkim1030 <kevin.kim1030@gmail.com>
2020-01-06 21:26:57 -0500

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ document.getElementById('main').remove()
 
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
+// newHeader.innerText = 'KEVIN is the champion'
 newHeader.innerHTML = 'KEVIN is the champion'
